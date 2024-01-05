ec32b1c54868d4e3e5de15a2e087dbe241b16adf
Nicholas Dimitroff <nicholas.dimitroff@gmail.com>
2024-01-05 09:41:50 -0800

    docstrings and functions are nice

--- examples.py
+++ examples.py
@@ -3,21 +3,19 @@ from typing import List
 from corduroy import (Computer, Cord, Keyboard, Monitor, Mouse, build_graph,
                       get_devices)
 
+def use_existing_devices():
+    """ Use existing devices from data/devices.txt to build your list """
+    devices: List = []
+    devices = get_devices(["test laptop", "usb mouse"])
+    G = build_graph(devices)
+    print(G)
 
-# Using the list of known devices
-
-devices: List = []
-devices = get_devices(["test laptop", "usb mouse"])
-G = build_graph(devices)
-print(G)
-
-# Creating your own devices
-
-devices: List = []
-devices.append(Mouse(name="mouse", cord="USB-A"))
-devices.append(Keyboard(name="keyboard", cord="USB-A"))
-devices.append(Computer(name="laptop", ports=["USB-A", "USB-A", "USB-C"]))
-devices.append(Monitor(name="monitor", ports=["USB-C"]))
-
-G = build_graph(devices)
-print(G)
+def use_custom_devices():
+    """ Create custom devices directly to build your list """
+    devices: List = []
+    devices.append(Mouse(name="mouse", cord="USB-A"))
+    devices.append(Keyboard(name="keyboard", cord="USB-A"))
+    devices.append(Computer(name="laptop", ports=["USB-A", "USB-A", "USB-C"]))
+    devices.append(Monitor(name="monitor", ports=["USB-C"]))
+    G = build_graph(devices)
+    print(G)
